c5ba2ff956085f0379a9d0fd9bf762459843a0a1
Joao Leonardo Pereira <joaoleonardo_pereira@apple.com>
2024-03-25 22:12:48 -0300

    Initial commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  numberInput: string = ''
   constructor() {}
 
+  openWhatsapp() {
+    let number = this.sanitizeNumber()
+    window.open(`https://wa.me/55${number}`, '_blank')
+  }
+
+  sanitizeNumber() {
+    this.numberInput = this.numberInput.replace(/\D/g, '')
+    return Number(this.numberInput).toString()
+  }
+
+  get isInputValid() {
+    const validPattern = /^\d{10,15}$/
+    const numbersOnly = this.sanitizeNumber()
+    
+    if (!validPattern.test(numbersOnly)) {
+      return false
+    }
+    return true
+  }
 }
