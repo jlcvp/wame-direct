d8755491ef4862b3becba21412c732ca1acd342e
Joao Leonardo Pereira <joaoleonardo_pereira@apple.com>
2024-03-27 20:47:52 -0300

    PWA setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
+import { AlertController, Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,63 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(
+    private platform: Platform,
+    private updates: SwUpdate,
+    private alertController: AlertController
+    ) {
+    // Splash screen handling
+    this.platform.ready().then(() => {
+      setTimeout(() => {
+        let splash = document.getElementById('splash-container')
+        if (splash != null) {
+          splash.style.opacity = '0'
+          setTimeout(() => {
+            splash?.remove()
+          }, 300);
+        }
+      }, 500);
+    }).catch(err => {
+      console.error("Error on platform.ready()", err)
+      setTimeout(() => {
+        let splash = document.getElementById('splash-container')
+        if (splash != null) {
+          splash.style.opacity = '0'
+          setTimeout(() => {
+            splash?.remove()
+          }, 300);
+        }
+      }, 500);
+    })
+
+    // PWA update handling
+    this.updates.versionUpdates.subscribe(async (evt) => {
+      switch (evt.type) {
+        case 'VERSION_DETECTED':
+          console.info(`Downloading new app version: ${evt.version.hash}`);
+          break;
+        case 'VERSION_READY':
+          console.info(`Current app version: ${evt.currentVersion.hash}`);
+          console.info(`New app version ready for use: ${evt.latestVersion.hash}`);
+          const alert = await this.alertController.create({
+            backdropDismiss: false,
+            header: 'Atualização disponível',
+            message: 'Uma nova versão da aplicação está disponível e será carregada automaticamente',
+            buttons: [
+              {
+                text: 'Atualizar',
+                handler: () => {
+                  document.location.reload()
+                }
+              }
+            ]
+          })
+          await alert.present()
+          break;
+        case 'VERSION_INSTALLATION_FAILED':
+          console.warn(`Failed to install new app version '${evt.version.hash}': ${evt.error}`);
+          break;
+      }
+    })
+  }
 }
